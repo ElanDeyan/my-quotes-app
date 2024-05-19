b70af96f33a8c729c01a324f59c34f7ebe5e50a5
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-05-18 22:25:28 -0300

    refactor: changes API

--- lib/repository/quotes_repository.dart
+++ lib/repository/quotes_repository.dart
@@ -3,15 +3,15 @@ import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
 abstract interface class QuotesRepository {
   Future<List<Quote>> get allQuotes;
 
-  Future<Quote> get randomQuote;
+  Future<Quote?> get randomQuote;
 
-  Stream<Quote?> getQuoteById(int id);
+  Future<Quote?> getQuoteById(int id);
 
   Future<int> addQuote(Quote quote);
 
-  Future<int> updateQuote(int id);
+  Future<bool> updateQuote(Quote quote);
 
   Future<int> removeQuote(int id);
 
-  Future<List<Quote>> get favorites;
+  Stream<List<Quote>> get favorites;
 }
