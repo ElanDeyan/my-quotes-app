62146881ac9fc515522258edc167d5099ea46995
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-05-20 15:02:45 -0300

    refactor: readjusts the tag table

--- lib/data/tables/tag_table.dart
+++ lib/data/tables/tag_table.dart
@@ -1,11 +1,8 @@
 import 'package:drift/drift.dart';
-import 'package:my_quotes/data/tables/quote_table.dart';
 
 @DataClassName('Tag')
 class TagTable extends Table {
-  IntColumn get id => integer().autoIncrement()();
+  IntColumn get id => integer().nullable().autoIncrement()();
 
-  TextColumn get tagName => text().withLength(min: 1)();
-
-  IntColumn get quotes => integer().nullable().references(QuoteTable, #id)();
+  TextColumn get name => text().withLength(min: 1)();
 }
