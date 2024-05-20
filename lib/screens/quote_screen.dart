5f5125fc05c45362e5cd7dcfc13afc62d76e5ab1
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-05-19 22:40:25 -0300

    feat: adds close and update buttons

--- lib/screens/quote_screen.dart
+++ lib/screens/quote_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_quotes/states/database_notifier.dart';
 import 'package:provider/provider.dart';
 
@@ -15,7 +16,14 @@ final class QuoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () => context.goNamed('mainScreen'),
+            icon: const Icon(Icons.close),
+          ),
+        ],
+      ),
       body: Center(
         child: Consumer<DatabaseNotifier>(
           builder: (context, database, child) => FutureBuilder(
@@ -28,6 +36,13 @@ final class QuoteScreen extends StatelessWidget {
                   children: [
                     const Text('Quote data'),
                     Text(data.toString()),
+                    ElevatedButton(
+                      onPressed: () => context.goNamed(
+                        'update',
+                        pathParameters: {'id': '$quoteId'},
+                      ),
+                      child: const Text('Update'),
+                    ),
                   ],
                 );
               } else {
