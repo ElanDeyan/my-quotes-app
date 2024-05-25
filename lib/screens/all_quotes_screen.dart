6a0a8544a7d973de1e7ead7f28e5c2708820a7d7
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-05-25 16:10:30 -0300

    feat: adds shimmer effect

--- lib/screens/all_quotes_screen.dart
+++ lib/screens/all_quotes_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_quotes/states/database_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer_pro/shimmer_pro.dart';
 
 final class AllQuotesScreen extends StatelessWidget {
   const AllQuotesScreen({
@@ -18,25 +19,56 @@ final class AllQuotesScreen extends StatelessWidget {
         builder: (context, snapshot) {
           final connectionState = snapshot.connectionState;
 
-          if (connectionState == ConnectionState.done && snapshot.hasData) {
-            final data = snapshot.data!;
-            return ListView.builder(
-              itemBuilder: (context, index) => ListTile(
-                title: Text(data[index].content),
-                subtitle: Text(data[index].author),
-                onTap: () => context.goNamed(
-                  'quote',
-                  pathParameters: {"id": "${data[index].id}"},
+          switch (connectionState) {
+            case ConnectionState.none:
+              return Text(
+                'No database found',
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+              );
+            case ConnectionState.active || ConnectionState.waiting:
+              final isDarkTheme =
+                  Theme.of(context).brightness == Brightness.dark;
+              final parentWidth = MediaQuery.of(context).size.width;
+
+              return Center(
+                child: ShimmerPro.generated(
+                  light: isDarkTheme ? ShimmerProLight.lighter : null,
+                  scaffoldBackgroundColor:
+                      Theme.of(context).colorScheme.primaryContainer,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        for (var i = 0; i < 10; i++)
+                          ShimmerPro.sized(
+                            scaffoldBackgroundColor: Theme.of(context)
+                                .colorScheme
+                                .onPrimaryContainer,
+                            height: 50,
+                            width: parentWidth,
+                          ),
+                      ],
+                    ),
+                  ),
                 ),
-                trailing: IconButton(
-                  onPressed: () => database.removeQuote(data[index].id!),
-                  icon: const Icon(Icons.delete_forever),
+              );
+            case ConnectionState.done:
+              final data = snapshot.data!;
+              return ListView.builder(
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(data[index].content),
+                  subtitle: Text(data[index].author),
+                  onTap: () => context.goNamed(
+                    'quote',
+                    pathParameters: {"id": "${data[index].id}"},
+                  ),
+                  trailing: IconButton(
+                    onPressed: () => database.removeQuote(data[index].id!),
+                    icon: const Icon(Icons.delete_forever),
+                  ),
                 ),
-              ),
-              itemCount: data.length,
-            );
-          } else {
-            return const CircularProgressIndicator();
+                itemCount: data.length,
+              );
           }
         },
       ),
