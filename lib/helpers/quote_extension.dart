d2edddc751da56fc6425989a90860b96bd67acb9
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-05-25 15:21:01 -0300

    feat: adds extensions for Quote class

---
+++ lib/helpers/quote_extension.dart
@@ -0,0 +1,10 @@
+import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
+import 'package:my_quotes/helpers/nullable_extension.dart';
+
+extension QuoteExtension on Quote {
+  bool get hasSourceAndUri => source.isNotNull && sourceUri.isNotNull;
+
+  bool get hasSource => source is String && source != '';
+
+  bool get hasSourceUri => sourceUri.isNotNull;
+}
