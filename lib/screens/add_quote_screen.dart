344c8485201673fac5ebf8a6c2fda018dfaa1c49
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-05-30 19:08:35 -0300

    refactor: unifies add and update in one form

--- lib/screens/add_quote_screen.dart
+++ lib/screens/add_quote_screen.dart
@@ -3,11 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:go_router/go_router.dart';
-import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
-import 'package:my_quotes/helpers/nullable_extension.dart';
-import 'package:my_quotes/helpers/url_pattern.dart';
-import 'package:my_quotes/states/database_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:my_quotes/constants/quote_form_mixin.dart';
 
 final class AddQuoteScreen extends StatelessWidget {
   const AddQuoteScreen({super.key});
@@ -49,9 +45,7 @@ final class AddQuoteForm extends StatefulWidget {
   State<AddQuoteForm> createState() => _AddQuoteFormState();
 }
 
-class _AddQuoteFormState extends State<AddQuoteForm> with UrlPattern {
-  final _formKey = GlobalKey<FormBuilderState>();
-
+class _AddQuoteFormState extends State<AddQuoteForm> with QuoteFormMixin {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -62,197 +56,10 @@ class _AddQuoteFormState extends State<AddQuoteForm> with UrlPattern {
             style: Theme.of(context).textTheme.headlineSmall,
           ),
           FormBuilder(
-            key: _formKey,
-            onChanged: _formKey.currentState?.validate,
+            key: formKey,
+            onChanged: formKey.currentState?.validate,
             autovalidateMode: AutovalidateMode.always,
-            child: Column(
-              children: [
-                FormBuilderTextField(
-                  name: 'content',
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Content',
-                  ),
-                  maxLines: null,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Can't be empty.";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                FormBuilderTextField(
-                  name: 'author',
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Author',
-                  ),
-                  initialValue: 'Anonym',
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return "Can't be empty.";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                FormBuilderTextField(
-                  name: 'source',
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Source',
-                    helperText: 'Movie, book, event, place and etc',
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                FormBuilderTextField(
-                  name: 'sourceUri',
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Source link',
-                    helperText: 'Link to the source',
-                  ),
-                  validator: (value) {
-                    if (value != null && value.isNotEmpty) {
-                      if (!urlPattern.hasMatch(value)) {
-                        return 'Enter a valid link';
-                      }
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                FormBuilderCheckbox(
-                  name: 'isFavorite',
-                  initialValue: false,
-                  title: const Text('Is favorite?'),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Consumer<DatabaseProvider>(
-                  builder: (context, value, child) {
-                    return FutureBuilder(
-                      future: value.allTags,
-                      initialData: const <Tag>[],
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          if (snapshot.hasData) {
-                            return FormBuilderFilterChip(
-                              name: 'tags',
-                              options: [
-                                for (final tag in snapshot.data!)
-                                  FormBuilderChipOption(
-                                    value: tag.id!.toString(),
-                                    child: Text(tag.name),
-                                  ),
-                              ],
-                            );
-                          } else {
-                            return const Text('No data');
-                          }
-                        } else {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      },
-                    );
-                  },
-                ),
-                Consumer<DatabaseProvider>(
-                  builder: (context, database, child) => ElevatedButton(
-                    onPressed: () async {
-                      final tagToAdd = await showDialog<String?>(
-                        context: context,
-                        builder: (context) {
-                          final textEditingController = TextEditingController();
-                          return AlertDialog(
-                            title: const Text('Create tag'),
-                            content: TextField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                              ),
-                              autofocus: true,
-                              onSubmitted: (value) =>
-                                  textEditingController.text = value,
-                              controller: textEditingController,
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text('Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.pop(
-                                  context,
-                                  textEditingController.text,
-                                ),
-                                child: const Text('Save'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-
-                      if (tagToAdd.isNotNull) {
-                        database.createTag(Tag(name: tagToAdd!));
-                      }
-                    },
-                    child: const Text('Create tag'),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Consumer<DatabaseProvider>(
-                  builder: (context, database, child) => ElevatedButton(
-                    child: const Text('Save'),
-                    onPressed: () {
-                      _formKey.currentState!.saveAndValidate();
-
-                      final formValue = _formKey.currentState!.value;
-
-                      final formValueMapped = formValue.map(
-                        (key, value) {
-                          if (key == 'tags') {
-                            value ??= <String>[];
-                            final newValue = (value as List<String>).join(',');
-                            return MapEntry(key, newValue);
-                          } else {
-                            return MapEntry(key, value);
-                          }
-                        },
-                      );
-
-                      try {
-                        final quoteToAdd = Quote.fromJson(formValueMapped);
-                        scheduleMicrotask(() => database.addQuote(quoteToAdd));
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("Succesfully added!")),
-                        );
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("Can't parse to Quote")),
-                        );
-                        rethrow;
-                      }
-
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-              ],
-            ),
+            child: quoteFormBody(),
           ),
         ],
       ),
--- lib/screens/update_quote_screen.dart
+++ lib/screens/update_quote_screen.dart
@@ -3,9 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_quotes/constants/quote_form_mixin.dart';
 import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
-import 'package:my_quotes/helpers/nullable_extension.dart';
-import 'package:my_quotes/helpers/url_pattern.dart';
 import 'package:my_quotes/states/database_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -104,15 +103,15 @@ class UpdateQuoteForm extends StatefulWidget {
   State<UpdateQuoteForm> createState() => _UpdateQuoteFormState();
 }
 
-class _UpdateQuoteFormState extends State<UpdateQuoteForm> with UrlPattern {
-  final _formKey = GlobalKey<FormBuilderState>();
+class _UpdateQuoteFormState extends State<UpdateQuoteForm> with QuoteFormMixin {
+  @override
+  bool get isUpdateForm => true;
 
   @override
   Widget build(BuildContext context) {
-    final database = Provider.of<DatabaseProvider>(context, listen: false);
     return FormBuilder(
-      key: _formKey,
-      onChanged: _formKey.currentState?.validate,
+      key: formKey,
+      onChanged: formKey.currentState?.validate,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       initialValue: widget.quote.toJson()
         ..update(
@@ -123,201 +122,7 @@ class _UpdateQuoteFormState extends State<UpdateQuoteForm> with UrlPattern {
           },
           ifAbsent: () => <String>[],
         ),
-      child: Column(
-        children: [
-          FormBuilderTextField(
-            name: 'content',
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Content',
-            ),
-            maxLines: null,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Can't be empty.";
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FormBuilderTextField(
-            name: 'author',
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Author',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Can't be empty.";
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FormBuilderTextField(
-            name: 'source',
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Source',
-              helperText: 'Movie, book, event, place and etc',
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FormBuilderTextField(
-            name: 'sourceUri',
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Source link',
-              helperText: 'Link to the source',
-            ),
-            validator: (value) {
-              if (value != null && value.isNotEmpty) {
-                if (!urlPattern.hasMatch(value)) {
-                  return 'Enter a valid link';
-                }
-              }
-              return null;
-            },
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FormBuilderCheckbox(
-            name: 'isFavorite',
-            title: const Text('Is favorite?'),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Consumer<DatabaseProvider>(
-            builder: (context, value, child) {
-              return FutureBuilder(
-                future: value.allTags,
-                initialData: const <Tag>[],
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    if (snapshot.hasData) {
-                      return FormBuilderFilterChip(
-                        name: 'tags',
-                        options: [
-                          for (final tag in snapshot.data!)
-                            FormBuilderChipOption(
-                              value: tag.id!.toString(),
-                              child: Text(tag.name),
-                            ),
-                        ],
-                      );
-                    } else {
-                      return const Text('No tags');
-                    }
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              );
-            },
-          ),
-          Consumer<DatabaseProvider>(
-            builder: (context, database, child) => ElevatedButton(
-              onPressed: () async {
-                final tagToAdd = await showDialog<String?>(
-                  context: context,
-                  builder: (context) {
-                    final textEditingController = TextEditingController();
-                    return AlertDialog(
-                      title: const Text('Create tag'),
-                      content: TextField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                        ),
-                        autofocus: true,
-                        onSubmitted: (value) =>
-                            textEditingController.text = value,
-                        controller: textEditingController,
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context),
-                          child: const Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.pop(
-                            context,
-                            textEditingController.text,
-                          ),
-                          child: const Text('Save'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-
-                if (tagToAdd.isNotNull) {
-                  database.createTag(Tag(name: tagToAdd!));
-                }
-              },
-              child: const Text('Create tag'),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          ElevatedButton(
-            child: const Text('Save'),
-            onPressed: () {
-              _formKey.currentState!.saveAndValidate();
-
-              final formValue = _formKey.currentState!.value;
-
-              final formValueMapped = formValue.map(
-                (key, value) {
-                  if (key == 'tags') {
-                    value ??= <String>[];
-                    final newValue = (value as List<String>).join(',');
-                    return MapEntry(key, newValue);
-                  } else {
-                    return MapEntry(key, value);
-                  }
-                },
-              );
-
-              formValueMapped.update(
-                'id',
-                (value) => value,
-                ifAbsent: () => widget.quote.id,
-              );
-
-              formValueMapped.update(
-                'createdAt',
-                (value) => value,
-                ifAbsent: () => widget.quote.createdAt,
-              );
-
-              try {
-                final quoteToUpdate = Quote.fromJson(formValueMapped);
-                scheduleMicrotask(() => database.updateQuote(quoteToUpdate));
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text("Succesfully updated!")),
-                );
-              } catch (e) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text("Can't parse to Quote")),
-                );
-                rethrow;
-              }
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
+      child: quoteFormBody(quoteForUpdate: widget.quote),
     );
   }
 }
