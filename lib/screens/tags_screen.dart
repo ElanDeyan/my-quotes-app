502e32974c1343281e40c62a8ead8d381bfd530c
elandeyandasilva <elandeyandasilva@gmail.com>
2024-05-31 16:52:54 +0000

    feat: allows to update tags

--- lib/screens/tags_screen.dart
+++ lib/screens/tags_screen.dart
@@ -1,4 +1,7 @@
+import 'package:basics/basics.dart';
 import 'package:flutter/material.dart';
+import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
+import 'package:my_quotes/screens/widgets/create_tag_dialog.dart';
 import 'package:my_quotes/states/database_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +13,19 @@ class TagsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tags'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await createTagDialog(context)?.then(
+            (value) {
+              if (value.isNotNullOrBlank) {
+                Provider.of<DatabaseProvider>(context, listen: false)
+                    .createTag(Tag(name: value!));
+              }
+            },
+          );
+        },
+        child: const Icon(Icons.new_label),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -36,9 +52,45 @@ class TagsScreen extends StatelessWidget {
                           alignment: WrapAlignment.spaceAround,
                           children: [
                             for (final tag in snapshot.data!)
-                              TextButton(
-                                onPressed: () {},
-                                child: Text(tag.name),
+                              ListTile(
+                                title: Text(tag.name),
+                                trailing: PopupMenuButton<Tag>(
+                                  itemBuilder: (context) => [
+                                    PopupMenuItem(
+                                      value: tag,
+                                      onTap: () async {
+                                        final newTagName =
+                                            await showUpdateTagDialog(
+                                          context,
+                                          tag,
+                                        );
+
+                                        if (newTagName.isNotNullOrBlank) {
+                                          database
+                                              .updateTag(
+                                                tag.copyWith(name: newTagName),
+                                              )
+                                              .then(
+                                                (value) => ScaffoldMessenger.of(
+                                                  context,
+                                                ).showSnackBar(
+                                                  const SnackBar(
+                                                    content: Text(
+                                                      'Successfully updated!',
+                                                    ),
+                                                  ),
+                                                ),
+                                              );
+                                        }
+                                      },
+                                      child: const Text('Update'),
+                                    ),
+                                    PopupMenuItem(
+                                      value: tag,
+                                      child: const Text('Delete'),
+                                    ),
+                                  ],
+                                ),
                               ),
                           ],
                         ),
@@ -47,6 +99,40 @@ class TagsScreen extends StatelessWidget {
             },
           ),
         ),
+      ),
+    );
+  }
+
+  Future<String?> showUpdateTagDialog(BuildContext context, Tag tag) {
+    final textEditingController =
+        TextEditingController.fromValue(TextEditingValue(text: tag.name));
+    final updateTagFormKey = GlobalKey<FormState>();
+    return showDialog<String?>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Update tag'),
+        content: Form(
+          key: updateTagFormKey,
+          autovalidateMode: AutovalidateMode.always,
+          child: TextFormField(
+            controller: textEditingController,
+            decoration: const InputDecoration(
+              labelText: 'New tag name',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) => value.isNullOrBlank ? 'Invalid value' : null,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, textEditingController.text),
+            child: const Text('Update'),
+          ),
+        ],
       ),
     );
   }
