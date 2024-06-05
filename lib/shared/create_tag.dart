82ec012e7ed4165d29cb9f26b2fbc4d48f39579b
elandeyandasilva <elandeyandasilva@gmail.com>
2024-06-05 00:40:44 +0000

    feat: creates a function to handle tag creation

---
+++ lib/shared/create_tag.dart
@@ -0,0 +1,23 @@
+import 'package:basics/basics.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
+import 'package:my_quotes/helpers/nullable_extension.dart';
+import 'package:my_quotes/shared/show_create_tag_dialog.dart';
+import 'package:my_quotes/states/database_provider.dart';
+import 'package:provider/provider.dart';
+
+void createTag(BuildContext context) {
+  final result = showCreateTagDialog(context);
+
+  if (result.isNotNull) {
+    result!.then((value) {
+      if (value.isNotNullOrBlank) {
+        final database = Provider.of<DatabaseProvider>(context, listen: false);
+        database.createTag(Tag(name: value!)).then((value) =>
+            FToast().init(context).showToast(child: const Text('Created!')),
+        );
+      }
+    });
+  }
+}
