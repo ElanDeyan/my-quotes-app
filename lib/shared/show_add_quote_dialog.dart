833ebe2839747de0fb6449d197427a1edd757264
elandeyandasilva <elandeyandasilva@gmail.com>
2024-06-05 02:33:58 +0000

    fix: adds dialog constructor

--- lib/shared/show_add_quote_dialog.dart
+++ lib/shared/show_add_quote_dialog.dart
@@ -5,6 +5,6 @@ import 'package:my_quotes/screens/add_quote_screen.dart';
 Future<void> showAddQuoteDialog(BuildContext context) {
   return showDialog(
     context: context,
-    builder: (context) => const AddQuoteScreen(),
+    builder: (context) => const Dialog.fullscreen(child: AddQuoteScreen()),
   );
 }
