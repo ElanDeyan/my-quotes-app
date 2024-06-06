fe260226ff88e480368450b30e0ff3d036a0b0b7
elandeyandasilva <elandeyandasilva@gmail.com>
2024-06-06 21:33:10 +0000

    feat: adds view quote action and reordenates actions

--- lib/shared/quote_actions.dart
+++ lib/shared/quote_actions.dart
@@ -1,5 +1,5 @@
-import 'package:basics/basics.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
 import 'package:my_quotes/helpers/quote_extension.dart';
 import 'package:my_quotes/screens/quote_screen.dart';
@@ -12,14 +12,19 @@ import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 enum QuoteActions {
+  info(icon: Icon(Icons.info), label: Text('Info'), name: 'Info'),
+  readMore(icon: Icon(Icons.visibility), label: Text('View quote'), name: 'View quote'),
   create(icon: Icon(Icons.create), label: Text('Create'), name: 'Create'),
   update(icon: Icon(Icons.edit), label: Text('Edit'), name: 'Edit'),
-  info(icon: Icon(Icons.info), label: Text('Info'), name: 'Info'),
-  delete(icon: Icon(Icons.delete), label: Text('Delete'), name: 'Delete'),
   copy(icon: Icon(Icons.copy), label: Text('Copy'), name: 'Copy'),
-  copyLink(icon: Icon(Icons.link), label: Text('Copy link'), name: 'Copy link'),
   share(icon: Icon(Icons.share), label: Text('Share'), name: 'Share'),
-  goToLink(icon: Icon(Icons.open_in_new), label: Text('Go to link'), name: 'Go to link');
+  copyLink(icon: Icon(Icons.link), label: Text('Copy link'), name: 'Copy link'),
+  goToLink(
+    icon: Icon(Icons.open_in_new),
+    label: Text('Go to link'),
+    name: 'Go to link',
+  ),
+  delete(icon: Icon(Icons.delete), label: Text('Delete'), name: 'Delete');
 
   const QuoteActions({
     required this.icon,
@@ -36,10 +41,11 @@ enum QuoteActions {
     return label.toString();
   }
 
-  static List<QuoteActions> availableActions(Quote quote, {
+  static List<QuoteActions> availableActions(
+    Quote quote, {
     Iterable<QuoteActions> actions = QuoteActions.values,
   }) =>
-      actions.where(quote.can).toList()..sortBy((action) => action.name);
+      actions.where(quote.can).toList();
 
   static PopupMenuButton<Quote> popupMenuButton(
     BuildContext context,
@@ -60,6 +66,8 @@ enum QuoteActions {
             (action) => PopupMenuItem(
               value: quote,
               onTap: switch (action) {
+                QuoteActions.readMore => () => context
+                    .pushNamed('quote', pathParameters: {'id': '${quote.id!}'}),
                 QuoteActions.create => () => showAddQuoteDialog(context),
                 QuoteActions.info => () => showQuoteInfoDialog(context, quote),
                 QuoteActions.delete => () => deleteQuote(context, quote),
