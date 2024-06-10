aa00d136f539e19e45b8166f7445ed1e9f11a595
elandeyandasilva <elandeyandasilva@gmail.com>
2024-06-10 15:08:22 +0000

    feat: shows tags, actions and enables tap to see quotes with this tag

--- lib/screens/tags_screen.dart
+++ lib/screens/tags_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
+import 'package:my_quotes/routes/routes_names.dart';
+import 'package:my_quotes/screens/search/search_tag_delegate.dart';
 import 'package:my_quotes/shared/create_tag.dart';
-import 'package:my_quotes/shared/delete_tag.dart';
-import 'package:my_quotes/shared/update_tag.dart';
+import 'package:my_quotes/shared/show_tag_search.dart';
+import 'package:my_quotes/shared/tag_actions.dart';
 import 'package:my_quotes/states/database_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +17,17 @@ class TagsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tags'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () =>
+                showTagSearch(context, SearchTagDelegate(context: context)),
+          ),
+        ],
         leading: BackButton(
           onPressed: () => context.canPop()
               ? context.pop(context)
-              : context.pushNamed('mainScreen'),
+              : context.pushNamed(myQuotesNavigationKey),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -26,7 +35,7 @@ class TagsScreen extends StatelessWidget {
         child: const Icon(Icons.new_label),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
         child: Consumer<DatabaseProvider>(
           builder: (context, database, child) => FutureBuilder(
             future: database.allTags,
@@ -44,7 +53,7 @@ class TagsScreen extends StatelessWidget {
                     ? const Center(
                         child: Text('Error'),
                       )
-                    : _tagsList(snapshot.data!),
+                    : _tagsList(context, snapshot.data!),
               };
             },
           ),
@@ -53,7 +62,7 @@ class TagsScreen extends StatelessWidget {
     );
   }
 
-  Widget _tagsList(List<Tag> tags) {
+  Widget _tagsList(BuildContext context, List<Tag> tags) {
     return ListView.builder(
       physics: const AlwaysScrollableScrollPhysics(),
       itemCount: tags.length,
@@ -71,21 +80,13 @@ class TagsScreen extends StatelessWidget {
       ),
       itemBuilder: (context, index) => ListTile(
         title: Text(tags[index].name),
-        trailing: Consumer<DatabaseProvider>(
-          builder: (context, database, child) => PopupMenuButton(
-            itemBuilder: (context) => [
-              PopupMenuItem<Tag>(
-                value: tags[index],
-                onTap: () => updateTag(context, tags[index]),
-                child: const Text('Update'),
-              ),
-              PopupMenuItem<Tag>(
-                value: tags[index],
-                onTap: () => deleteTag(context, tags[index]),
-                child: const Text('Delete'),
-              ),
-            ],
-          ),
+        onTap: () => context.goNamed(quoteWithTagNavigationKey, pathParameters:{
+          'tagId': '${tags[index].id!}',
+        },),
+        trailing: PopupMenuButton(
+          tooltip: 'Actions',
+          itemBuilder: (context) =>
+              TagActions.popupMenuItems(context, tags[index]),
         ),
       ),
     );
