6ef9e61cde57258c7dfaf0799e8bdb5627fe8c33
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-06-18 23:04:10 -0300

    feat: partial translations

--- lib/screens/quote_screen.dart
+++ lib/screens/quote_screen.dart
@@ -1,13 +1,14 @@
 import 'package:basics/basics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
 import 'package:my_quotes/helpers/nullable_extension.dart';
 import 'package:my_quotes/helpers/quote_extension.dart';
 import 'package:my_quotes/routes/routes_names.dart';
-import 'package:my_quotes/shared/quote_card.dart';
-import 'package:my_quotes/shared/show_update_quote_dialog.dart';
+import 'package:my_quotes/shared/actions/quotes/show_update_quote_dialog.dart';
+import 'package:my_quotes/shared/widgets/quote_card.dart';
 import 'package:my_quotes/states/database_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:shimmer_pro/shimmer_pro.dart';
@@ -20,13 +21,11 @@ final class QuoteScreen extends StatelessWidget {
 
   final int quoteId;
 
-  static const screenName = 'Quote';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Quote info'),
+        title: Text(AppLocalizations.of(context)!.quoteInfo),
         leading: BackButton(
           onPressed: () => context.canPop()
               ? context.pop()
@@ -44,7 +43,7 @@ Future<void> showQuoteInfoDialog(BuildContext context, Quote quote) {
     builder: (context) => AlertDialog(
       icon: const Icon(Icons.info),
       scrollable: true,
-      title: const Text('Quote info'),
+      title: Text(AppLocalizations.of(context)!.quoteInfo),
       content: ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 500),
         child: QuoteScreenDialogBody(quoteId: quote.id!),
@@ -55,9 +54,9 @@ Future<void> showQuoteInfoDialog(BuildContext context, Quote quote) {
             Navigator.pop(context);
             showUpdateQuoteDialog(context, quote);
           },
-          child: const Text('Edit'),
+          child: Text(AppLocalizations.of(context)!.edit),
         ),
-        OutlinedButton(onPressed: () => context.pop(), child: const Text('Ok')),
+        OutlinedButton(onPressed: () => context.pop(), child: Text(AppLocalizations.of(context)!.ok)),
       ],
     ),
   );
@@ -92,7 +91,7 @@ class ViewQuotePage extends StatelessWidget {
           switch (connectionState) {
             case ConnectionState.none:
               return Text(
-                'No database found',
+                AppLocalizations.of(context)!.noDatabaseConnectionMessage,
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.onPrimary,
                 ),
