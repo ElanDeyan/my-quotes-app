1d0ca78a8f64273b82a4571fe50c76bacde2ee4e
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-07-07 23:08:30 -0300

    test: tests the brightness of color scheme

---
+++ test/enums/color_scheme_pallete_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_quotes/constants/enums/color_scheme_pallete.dart';
+
+void main() {
+  test('Is light color scheme?', () {
+    final randomColorSchemes = List.of(ColorSchemePalette.values)..shuffle();
+
+    for (final colorSchemePalette in randomColorSchemes) {
+      expect(
+        ColorSchemePalette.lightColorScheme(colorSchemePalette).brightness ==
+            Brightness.light,
+        isTrue,
+      );
+    }
+  });
+
+  test('Is dark color scheme?', () {
+    final randomColorSchemes = List.of(ColorSchemePalette.values)..shuffle();
+
+    for (final colorSchemePalette in randomColorSchemes) {
+      expect(
+        ColorSchemePalette.darkColorScheme(colorSchemePalette).brightness ==
+            Brightness.dark,
+        isTrue,
+      );
+    }
+  });
+}
