846878a6f46546f01d71831bcef1178394ee32ba
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-07-17 22:35:01 -0300

    feat: new operations in quotes dao and create tags dao

--- lib/data/local/db/daos/quotes_dao.dart
+++ lib/data/local/db/daos/quotes_dao.dart
@@ -1,6 +1,8 @@
+import 'package:basics/basics.dart';
 import 'package:drift/drift.dart';
 import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
 import 'package:my_quotes/data/tables/quote_table.dart';
+import 'package:my_quotes/helpers/quote_extension.dart';
 
 part 'quotes_dao.g.dart';
 
@@ -9,5 +11,17 @@ class QuotesDao extends DatabaseAccessor<AppDatabase> with _$QuotesDaoMixin {
   QuotesDao(super.attachedDatabase);
 
   Future<List<Quote>> get allQuotes => select(quoteTable).get();
-  
+
+  Future<Quote?> getQuoteById(int id) {
+    return (select(quoteTable)..where((row) => row.id.equals(id)))
+        .getSingleOrNull();
+  }
+
+  Future<List<Quote>> getQuotesWithTagId(int tagId) async {
+    final quotes = await allQuotes;
+
+    return quotes.where((quote) => quote.tagsId.contains(tagId)).toList();
+  }
+
+  Future<Quote?> get randomQuote async => (await allQuotes).getRandom();
 }
