a6ac07c3e380d81f25e825d76959079aa05ec364
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-08-05 15:01:35 -0300

    refactor: moves this showDialog to another file

--- lib/services/handle_backup_file.dart
+++ lib/services/handle_backup_file.dart
@@ -6,6 +6,7 @@ import 'package:my_quotes/helpers/string_to_theme_mode.dart';
 import 'package:my_quotes/repository/user_preferences_interfaces.dart';
 import 'package:my_quotes/services/parse_backup_file.dart';
 import 'package:my_quotes/shared/actions/show_toast.dart';
+import 'package:my_quotes/shared/actions/show_wants_to_import_dialog.dart';
 import 'package:my_quotes/shared/widgets/pill_chip.dart';
 import 'package:my_quotes/states/app_preferences.dart';
 import 'package:my_quotes/states/database_provider.dart';
@@ -18,31 +19,7 @@ Future<void> handleBackupFile(BuildContext context, XFile? file) async {
 
     if (backupData != null) {
       if (context.mounted) {
-        final wantsToImport = await showDialog<bool>(
-          context: context,
-          builder: (context) => AlertDialog(
-            actions: [
-              TextButton.icon(
-                onPressed: () => Navigator.pop(context, false),
-                label: Text(AppLocalizations.of(context)!.cancel),
-              ),
-              TextButton.icon(
-                onPressed: () => Navigator.pop(context, true),
-                label: Text(AppLocalizations.of(context)!.restoreData),
-              ),
-            ],
-            icon: const Icon(Icons.backup_outlined),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(AppLocalizations.of(context)!.restoreQuestion),
-                Text(
-                  AppLocalizations.of(context)!.restoreAdvice,
-                ),
-              ],
-            ),
-          ),
-        );
+        final wantsToImport = await showWantsToImportDialog(context);
 
         if (wantsToImport ?? false) {
           if (context.mounted) {
---
+++ lib/shared/actions/show_wants_to_import_dialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+Future<bool?> showWantsToImportDialog(BuildContext context) => showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+          actions: [
+            TextButton.icon(
+              onPressed: () => Navigator.pop(context, false),
+              label: Text(AppLocalizations.of(context)!.cancel),
+            ),
+            TextButton.icon(
+              onPressed: () => Navigator.pop(context, true),
+              label: Text(AppLocalizations.of(context)!.restoreData),
+            ),
+          ],
+          icon: const Icon(Icons.backup_outlined),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(AppLocalizations.of(context)!.restoreQuestion),
+              Text(
+                AppLocalizations.of(context)!.restoreAdvice,
+              ),
+            ],
+          ),
+        ),
+      );
