dfaa82596f30ab75030111bb18157c0732b385b5
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-08-06 23:15:40 -0300

    fix: refactors to add _textEditingController dispose call

--- lib/shared/actions/tags/show_create_tag_dialog.dart
+++ lib/shared/actions/tags/show_create_tag_dialog.dart
@@ -11,40 +11,74 @@ Future<String?>? showCreateTagDialog(
   return await showDialog<String?>(
     context: context,
     builder: (context) {
-      late final TextEditingController textEditingController;
-
-      if (initialValue.isNullOrBlank) {
-        textEditingController = TextEditingController();
-      } else {
-        textEditingController = TextEditingController(text: initialValue);
-      }
-
-      final createTagFormKey = GlobalKey<FormState>();
-      return AlertDialog(
-        title: Text(AppLocalizations.of(context)!.createTag),
-        content: Form(
-          key: createTagFormKey,
-          autovalidateMode: AutovalidateMode.always,
-          child: TagNameField(textEditingController: textEditingController),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text(AppLocalizations.of(context)!.cancel),
-          ),
-          TextButton(
-            onPressed: () {
-              if (createTagFormKey.currentState?.validate() ?? false) {
-                Navigator.pop(
-                  context,
-                  removeDiacritics(textEditingController.text.trim()),
-                );
-              }
-            },
-            child: Text(AppLocalizations.of(context)!.save),
-          ),
-        ],
+      return CreateTagDialog(
+        initialValue: initialValue,
       );
     },
   );
+}
+
+class CreateTagDialog extends StatefulWidget {
+  const CreateTagDialog({
+    super.key,
+    required this.initialValue,
+  });
+
+  final String? initialValue;
+
+  @override
+  State<CreateTagDialog> createState() => _CreateTagDialogState();
+}
+
+class _CreateTagDialogState extends State<CreateTagDialog> {
+  final GlobalKey<FormState> _createTagFormKey = GlobalKey<FormState>();
+
+  late final TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.initialValue.isNullOrBlank) {
+      _textEditingController = TextEditingController();
+    } else {
+      _textEditingController = TextEditingController.fromValue(
+        TextEditingValue(text: widget.initialValue!),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(AppLocalizations.of(context)!.createTag),
+      content: Form(
+        key: _createTagFormKey,
+        autovalidateMode: AutovalidateMode.always,
+        child: TagNameField(textEditingController: _textEditingController),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text(AppLocalizations.of(context)!.cancel),
+        ),
+        TextButton(
+          onPressed: () {
+            if (_createTagFormKey.currentState?.validate() ?? false) {
+              Navigator.pop(
+                context,
+                removeDiacritics(_textEditingController.text.trim()),
+              );
+            }
+          },
+          child: Text(AppLocalizations.of(context)!.save),
+        ),
+      ],
+    );
+  }
 }
--- lib/shared/actions/tags/show_update_tag_dialog.dart
+++ lib/shared/actions/tags/show_update_tag_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:basics/string_basics.dart';
 import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -5,17 +6,57 @@ import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
 import 'package:my_quotes/shared/widgets/tag_name_field.dart';
 
 Future<String?> showUpdateTagDialog(BuildContext context, Tag tag) {
-  final textEditingController =
-      TextEditingController.fromValue(TextEditingValue(text: tag.name));
-  final updateTagFormKey = GlobalKey<FormState>();
   return showDialog<String?>(
     context: context,
-    builder: (context) => AlertDialog(
+    builder: (context) => UpdateTagDialog(
+      tagName: tag.name,
+    ),
+  );
+}
+
+class UpdateTagDialog extends StatefulWidget {
+  const UpdateTagDialog({
+    super.key,
+    required this.tagName,
+  });
+
+  final String tagName;
+
+  @override
+  State<UpdateTagDialog> createState() => _UpdateTagDialogState();
+}
+
+class _UpdateTagDialogState extends State<UpdateTagDialog> {
+  final _updateTagFormKey = GlobalKey<FormState>();
+
+  late final TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.tagName.isNotNullOrBlank) {
+      _textEditingController = TextEditingController.fromValue(
+        TextEditingValue(text: widget.tagName),
+      );
+    } else {
+      _textEditingController = TextEditingController();
+    }
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
       title: Text(AppLocalizations.of(context)!.updateTag),
       content: Form(
-        key: updateTagFormKey,
+        key: _updateTagFormKey,
         autovalidateMode: AutovalidateMode.always,
-        child: TagNameField(textEditingController: textEditingController),
+        child: TagNameField(textEditingController: _textEditingController),
       ),
       actions: [
         TextButton(
@@ -24,16 +65,16 @@ Future<String?> showUpdateTagDialog(BuildContext context, Tag tag) {
         ),
         TextButton(
           onPressed: () {
-            if (updateTagFormKey.currentState?.validate() ?? false) {
+            if (_updateTagFormKey.currentState?.validate() ?? false) {
               Navigator.pop(
                 context,
-                removeDiacritics(textEditingController.text.trim()),
+                removeDiacritics(_textEditingController.text.trim()),
               );
             }
           },
           child: Text(AppLocalizations.of(context)!.save),
         ),
       ],
-    ),
-  );
+    );
+  }
 }
