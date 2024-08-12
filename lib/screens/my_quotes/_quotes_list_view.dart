c5baf2d2328611e93bb10eaa198ac685521ed356
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-08-11 23:48:26 -0300

    refactor: shows recent items first

--- lib/screens/my_quotes/_quotes_list_view.dart
+++ lib/screens/my_quotes/_quotes_list_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_quotes/data/local/db/quotes_drift_database.dart';
 import 'package:my_quotes/screens/my_quotes/_quote_tile.dart';
 
-class QuotesListView extends StatelessWidget {
+class QuotesListView extends StatefulWidget {
   const QuotesListView({
     super.key,
     required this.data,
@@ -11,15 +11,20 @@ class QuotesListView extends StatelessWidget {
   final List<Quote> data;
 
   @override
+  State<QuotesListView> createState() => _QuotesListViewState();
+}
+
+class _QuotesListViewState extends State<QuotesListView> {
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
       physics: const AlwaysScrollableScrollPhysics(),
-      itemCount: data.length,
-      semanticChildCount: data.length,
+      itemCount: widget.data.length,
+      semanticChildCount: widget.data.length,
       padding: const EdgeInsets.only(
         bottom: kBottomNavigationBarHeight + kFloatingActionButtonMargin * 2,
       ),
-      itemBuilder: (context, index) => QuoteTile(quote: data[index]),
+      itemBuilder: (context, index) => QuoteTile(quote: widget.data[index]),
     );
   }
 }
