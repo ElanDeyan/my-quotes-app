441a55e699f9a1638e075085875510f9756410f3
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-08-16 14:15:49 -0300

    refactor: better skeleton after changes

--- lib/screens/my_quotes/_quotes_list_view_skeleton.dart
+++ lib/screens/my_quotes/_quotes_list_view_skeleton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_quotes/shared/widgets/gap.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
 class QuotesListViewSkeleton extends StatelessWidget {
@@ -10,20 +11,41 @@ class QuotesListViewSkeleton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Skeletonizer(
       ignoreContainers: true,
-      child: ListView(
-        physics: const AlwaysScrollableScrollPhysics(),
-        children: List.generate(
-          10,
-          (index) => const ListTile(
-            leading: Bone.icon(),
-            trailing: Skeleton.shade(child: Icon(Icons.more_vert_outlined)),
-            title: Bone.multiText(),
-            subtitle: Bone.text(
-              words: 5,
+      child: CustomScrollView(
+        clipBehavior: Clip.none,
+        slivers: [
+          const SliverToBoxAdapter(
+            child: Gap.vertical(spacing: 5),
+          ),
+          SliverToBoxAdapter(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 10,
+                children: List.generate(
+                  2,
+                  (index) => const Bone.button(),
+                  growable: false,
+                ),
+              ),
             ),
           ),
-          growable: false,
-        ),
+          SliverList.list(
+            children: List.generate(
+              10,
+              (index) => const ListTile(
+                leading: Bone.icon(),
+                trailing: Skeleton.shade(child: Icon(Icons.more_vert_outlined)),
+                title: Bone.multiText(),
+                subtitle: Bone.text(
+                  words: 5,
+                ),
+              ),
+              growable: false,
+            ),
+          ),
+        ],
       ),
     );
   }
