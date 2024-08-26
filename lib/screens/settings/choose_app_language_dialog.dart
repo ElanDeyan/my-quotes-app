20104992b6d0bbde0bb05815fcf92a2909c86d0f
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-08-25 23:23:30 -0300

    fix: large dialog in large screens
    Made using the simple dialog widget

---
+++ lib/screens/settings/choose_app_language_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:my_quotes/helpers/build_context_extension.dart';
+import 'package:my_quotes/repository/user_preferences_interfaces.dart';
+import 'package:my_quotes/states/app_preferences.dart';
+import 'package:provider/provider.dart';
+
+class ChooseAppLanguageDialog extends StatelessWidget {
+  const ChooseAppLanguageDialog({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final appPreferences = Provider.of<AppPreferences>(context, listen: false);
+    
+    return SimpleDialog(
+      clipBehavior: Clip.none,
+      title: Text(context.appLocalizations.chooseAppLanguage),
+      children: List.generate(
+        LanguageRepository.values.length,
+        (index) {
+          final language = LanguageRepository.values[index];
+
+          return SimpleDialogOption(
+            onPressed: () => appPreferences.language = language,
+            child: Text(
+              context.appLocalizations.languageName(language),
+              softWrap: true,
+            ),
+          );
+        },
+        growable: false,
+      ),
+    );
+  }
+}
