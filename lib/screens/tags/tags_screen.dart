e63656ab046a613837ae4a648607c01c4b1aed0d
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-08-26 22:02:54 -0300

    style: formats

--- lib/screens/tags/tags_screen.dart
+++ lib/screens/tags/tags_screen.dart
@@ -61,7 +61,8 @@ class TagsScreen extends StatelessWidget {
 
             return switch ((connectionState, hasError, hasData)) {
               (ConnectionState.none, _, _) => const NoDatabaseConnectionMessage(
-                  key: Key('no_database_connection_message')),
+                  key: Key('no_database_connection_message'),
+                ),
               (ConnectionState.waiting, _, _) => const TagsListViewSkeleton(),
               (ConnectionState.active || ConnectionState.done, _, true)
                   when data!.isNotEmpty =>
