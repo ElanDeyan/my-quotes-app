9d7fcc685db0e0e518e66a7df86b4eb2343947df
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-05 18:04:38 -0300

    fix: stack was taking parent's area

--- lib/shared/widgets/quote_card/quote_card.dart
+++ lib/shared/widgets/quote_card/quote_card.dart
@@ -21,148 +21,150 @@ class QuoteCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final appLocalizations = context.appLocalizations;
 
-    return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxWidth: 500,
-        maxHeight: 250,
-      ),
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Card.outlined(
-            clipBehavior: Clip.none,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusDirectional.circular(10),
-              side: BorderSide(
-                color: Theme.of(context).colorScheme.primary,
+    return Center(
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 500,
+          maxHeight: 250,
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Card.outlined(
+              clipBehavior: Clip.none,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(10),
+                side: BorderSide(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ),
-            ),
-            color: Theme.of(context).colorScheme.surfaceContainerLowest,
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 26.0,
-                right: 26.0,
-                top: 22.0,
-                bottom: 11.0,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Flexible(
-                    child: SingleChildScrollView(
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      child: AnimatedSize(
-                        duration: Durations.short4,
-                        child: Text(
-                          quote.content,
-                          softWrap: true,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontStyle: FontStyle.italic,
-                            fontSize:
-                                Theme.of(context).textTheme.bodyLarge!.fontSize,
+              color: Theme.of(context).colorScheme.surfaceContainerLowest,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 26.0,
+                  right: 26.0,
+                  top: 22.0,
+                  bottom: 11.0,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: SingleChildScrollView(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        child: AnimatedSize(
+                          duration: Durations.short4,
+                          child: Text(
+                            quote.content,
+                            softWrap: true,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontSize:
+                                  Theme.of(context).textTheme.bodyLarge!.fontSize,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  AnimatedSize(
-                    duration: Durations.short4,
-                    child: Text(
-                      '- ${quote.author}'
-                      '${quote.hasSource ? ', ${quote.source}.' : '.'}',
-                      softWrap: true,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize:
-                            Theme.of(context).textTheme.bodySmall!.fontSize,
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    AnimatedSize(
+                      duration: Durations.short4,
+                      child: Text(
+                        '- ${quote.author}'
+                        '${quote.hasSource ? ', ${quote.source}.' : '.'}',
+                        softWrap: true,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize:
+                              Theme.of(context).textTheme.bodySmall!.fontSize,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  AnimatedSize(
-                    duration: Durations.short4,
-                    child: FutureBuilder(
-                      future: serviceLocator<AppRepository>()
-                          .getTagsByIds(quote.tagsId),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.done) {
-                          if (!snapshot.hasError) {
-                            return Wrap(
-                              runSpacing: 5.0,
-                              spacing: 5.0,
-                              children: [
-                                for (final tag in snapshot.data!)
-                                  Text(
-                                    '#${tag.name}',
-                                    style: TextStyle(
-                                      fontSize: Theme.of(context)
-                                          .textTheme
-                                          .labelSmall!
-                                          .fontSize,
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    AnimatedSize(
+                      duration: Durations.short4,
+                      child: FutureBuilder(
+                        future: serviceLocator<AppRepository>()
+                            .getTagsByIds(quote.tagsId),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState == ConnectionState.done) {
+                            if (!snapshot.hasError) {
+                              return Wrap(
+                                runSpacing: 5.0,
+                                spacing: 5.0,
+                                children: [
+                                  for (final tag in snapshot.data!)
+                                    Text(
+                                      '#${tag.name}',
+                                      style: TextStyle(
+                                        fontSize: Theme.of(context)
+                                            .textTheme
+                                            .labelSmall!
+                                            .fontSize,
+                                      ),
                                     ),
-                                  ),
-                              ],
-                            );
+                                ],
+                              );
+                            }
                           }
-                        }
-                        return const SizedBox.shrink();
+                          return const SizedBox.shrink();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: -2.25,
+              right: -2.25,
+              child: AnimatedSwitcher(
+                duration: Durations.short4,
+                child: IconButton(
+                  onPressed: () {},
+                  iconSize: 16,
+                  icon: Icon(
+                    quote.isFavorite ? Icons.favorite : Icons.favorite_border,
+                    size: 16,
+                  ),
+                ),
+              ),
+            ),
+            if (showActions)
+              Positioned(
+                top: -3.75,
+                right: -1,
+                child: PopupMenuButton(
+                  tooltip: appLocalizations.quoteActionsPopupButtonTooltip,
+                  icon: const Icon(Icons.more_horiz_outlined),
+                  position: PopupMenuPosition.under,
+                  itemBuilder: (context) => QuoteActions.popupMenuItems(
+                    appLocalizations,
+                    serviceLocator<AppRepository>(),
+                    context,
+                    quote,
+                    actions: QuoteActions.values.where(
+                      (action) => switch (action) {
+                        QuoteActions.create || QuoteActions.delete => false,
+                        _ => true
                       },
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: -2.25,
-            right: -2.25,
-            child: AnimatedSwitcher(
-              duration: Durations.short4,
-              child: IconButton(
-                onPressed: () {},
-                iconSize: 16,
-                icon: Icon(
-                  quote.isFavorite ? Icons.favorite : Icons.favorite_border,
-                  size: 16,
                 ),
               ),
+            const Positioned(
+              top: -16.25,
+              left: -16.25,
+              child: QuoteIconDecoration(),
             ),
-          ),
-          if (showActions)
-            Positioned(
-              top: -3.75,
-              right: -1,
-              child: PopupMenuButton(
-                tooltip: appLocalizations.quoteActionsPopupButtonTooltip,
-                icon: const Icon(Icons.more_horiz_outlined),
-                position: PopupMenuPosition.under,
-                itemBuilder: (context) => QuoteActions.popupMenuItems(
-                  appLocalizations,
-                  serviceLocator<AppRepository>(),
-                  context,
-                  quote,
-                  actions: QuoteActions.values.where(
-                    (action) => switch (action) {
-                      QuoteActions.create || QuoteActions.delete => false,
-                      _ => true
-                    },
-                  ),
-                ),
-              ),
-            ),
-          const Positioned(
-            top: -16.25,
-            left: -16.25,
-            child: QuoteIconDecoration(),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
