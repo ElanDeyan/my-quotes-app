e31d1fb4d848250eb726dcbf8503f4766fb969aa
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-05 11:28:55 -0300

    fix: corrects layout when no quote is added yet

--- lib/screens/home/random_quote_section.dart
+++ lib/screens/home/random_quote_section.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:my_quotes/repository/interfaces/app_repository.dart';
 import 'package:my_quotes/screens/home/random_quote_container.dart';
+import 'package:my_quotes/services/service_locator.dart';
 import 'package:my_quotes/shared/widgets/an_error_occurred_message.dart';
 import 'package:my_quotes/shared/widgets/no_database_connection_message.dart';
 import 'package:my_quotes/shared/widgets/no_quotes_added_yet_message.dart';
 import 'package:my_quotes/shared/widgets/quote_card/quote_card_skeleton.dart';
-import 'package:my_quotes/services/service_locator.dart';
 
 class RandomQuoteSection extends StatelessWidget {
   const RandomQuoteSection({super.key});
@@ -27,10 +27,10 @@ class RandomQuoteSection extends StatelessWidget {
               child: QuoteCardSkeleton(key: Key('quote_card_skeleton')),
             ),
           (ConnectionState.active || ConnectionState.done, _, true)
-              when data == null =>
+              when data != null && data.isEmpty =>
             const NoQuotesAddedYetMessage(),
           (ConnectionState.active || ConnectionState.done, _, true)
-              when data != null =>
+              when data != null && data.isNotEmpty =>
             RandomQuoteContainer(
               key: const Key('random_quote_container'),
               quotes: data..shuffle(),
