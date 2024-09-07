9ca52b0c401533302b39257250833f6933881e31
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-07 18:34:39 -0300

    refactor: adds constant for file extension and consumes a stream instead of a future when getting backup file

--- lib/services/get_backup_file.dart
+++ lib/services/get_backup_file.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:file_picker/file_picker.dart';
+import 'package:my_quotes/constants/backup_file_extension.dart';
 import 'package:path/path.dart' as p;
 import 'package:share_plus/share_plus.dart';
 
@@ -7,18 +10,24 @@ Future<XFile?> getBackupFile() async {
     lockParentWindow: true,
     type: FileType.custom,
     allowedExtensions: ['txt'],
-    withData: true,
+    withReadStream: true,
   );
 
   if (result != null) {
     final file = result.files.single;
 
-    if (p.extension(file.name, 2) != '.myquotes.txt') {
+    if (p.extension(file.name, 2) != backupFileExtension) {
       return null;
     }
 
-    if (file.bytes != null) {
-      return XFile.fromData(file.bytes!);
+    if (file.readStream case final Stream<List<int>> bytesStream) {
+      final fileData = <int>[];
+
+      await for (final data in bytesStream) {
+        fileData.addAll(data);
+      }
+
+      return XFile.fromData(Uint8List.fromList(fileData));
     }
   }
   return null;
