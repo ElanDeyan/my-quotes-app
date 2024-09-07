33d8856f159080d2dde70b1d76f38a130a4fc3ce
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-07 17:45:31 -0300

    fix: corrects file extension for mobile, and adds validation for .myquotes.txt extension

--- lib/services/get_backup_file.dart
+++ lib/services/get_backup_file.dart
@@ -1,19 +1,24 @@
 import 'package:file_picker/file_picker.dart';
+import 'package:path/path.dart' as p;
 import 'package:share_plus/share_plus.dart';
 
 Future<XFile?> getBackupFile() async {
   final FilePickerResult? result = await FilePicker.platform.pickFiles(
     lockParentWindow: true,
     type: FileType.custom,
-    allowedExtensions: ['myquotes.txt'],
+    allowedExtensions: ['txt'],
     withData: true,
   );
 
   if (result != null) {
-    final fileBytes = result.files.single.bytes;
+    final file = result.files.single;
 
-    if (fileBytes != null) {
-      return XFile.fromData(fileBytes);
+    if (p.extension(file.name, 2) != '.myquotes.txt') {
+      return null;
+    }
+
+    if (file.bytes != null) {
+      return XFile.fromData(file.bytes!);
     }
   }
   return null;
