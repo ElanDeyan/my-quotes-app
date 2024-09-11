27304265fdbf9692033964c1af95b4a2487f9906
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-09-10 23:02:28 -0300

    chore: adds sqlcipher_flutter_libs
    These changes intent to add this to native database files.
    
    They aren't working

--- lib/data/local/db/connection/native.dart
+++ lib/data/local/db/connection/native.dart
@@ -5,15 +5,17 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-// import 'package:sqlcipher_flutter_libs/sqlcipher_flutter_libs.dart';
+import 'package:sqlcipher_flutter_libs/sqlcipher_flutter_libs.dart';
 import 'package:sqlite3/open.dart';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
+const _encryptionPassword = 'my_unsafe_password';
+
 Future<File> get databaseFile async {
   final appDir = await getApplicationDocumentsDirectory();
-  // final dbPath = p.join(appDir.path, 'my_quotes.db.enc');
-  final dbPath = p.join(appDir.path, 'my_quotes.db');
+  final dbPath = p.join(appDir.path, 'my_quotes.db.enc');
+  // final dbPath = p.join(appDir.path, 'my_quotes.db');
 
   return File(dbPath);
 }
@@ -31,35 +33,35 @@ DatabaseConnection connect() {
 
       return NativeDatabase.createBackgroundConnection(
         await databaseFile,
-        // isolateSetup: () async => open
-        //   ..overrideFor(
-        //     OperatingSystem.android,
-        //     openCipherOnAndroid,
-        //   )
-        //   ..overrideFor(
-        //     OperatingSystem.linux,
-        //     () => DynamicLibrary.open('libsqlcipher.so'),
-        //   )
-        //   ..overrideFor(
-        //     OperatingSystem.windows,
-        //     () => DynamicLibrary.open('sqlcipher.dll'),
-        //   ),
-        // setup: (database) {
-        //   final result = database.select('pragma cipher_version');
+        isolateSetup: () async => open
+          ..overrideFor(
+            OperatingSystem.android,
+            openCipherOnAndroid,
+          )
+          ..overrideFor(
+            OperatingSystem.linux,
+            () => DynamicLibrary.open('libsqlcipher.so'),
+          )
+          ..overrideFor(
+            OperatingSystem.windows,
+            () => DynamicLibrary.open('sqlcipher.dll'),
+          ),
+        setup: (database) {
+          final result = database.select('pragma cipher_version');
 
-        //   if (result.isEmpty) {
-        //     throw UnsupportedError(
-        //       'This database needs to run with SQLCipher, but that library is '
-        //       'not available!',
-        //     );
-        //   }
+          if (result.isEmpty) {
+            throw UnsupportedError(
+              'This database needs to run with SQLCipher, but that library is '
+              'not available!',
+            );
+          }
 
-        //   final escapedKey = _encryptionPassword.replaceAll("'", "''");
-        //   database.execute('pragma key = $escapedKey');
+          final escapedKey = _encryptionPassword.replaceAll("'", "''");
+          database.execute('pragma key = $escapedKey');
 
-        //   // Tests that the key is correct by selecting from a table
-        //   database.execute('select count(*) from sqlite_master');
-        // },
+          // Tests that the key is correct by selecting from a table
+          database.execute('select count(*) from sqlite_master');
+        },
       );
     }),
   );
