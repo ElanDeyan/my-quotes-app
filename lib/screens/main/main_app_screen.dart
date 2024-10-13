5a0c954878f59f3b19a970ce42844debccd875a5
Elan Deyan da Silva Almeida <elandeyandasilva@gmail.com>
2024-10-13 18:10:16 -0300

    refactor: removes unused mixin and reorders arguments

--- lib/screens/main/main_app_screen.dart
+++ lib/screens/main/main_app_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:my_quotes/screens/main/compact_window_width_main_app_screen.dart';
-import 'package:my_quotes/screens/main/destinations.dart';
 import 'package:my_quotes/screens/main/large_window_width_main_app_screen.dart';
 
-class MainAppScreen extends StatelessWidget with DestinationsMixin {
-  const MainAppScreen({super.key, required this.initialIndex});
+class MainAppScreen extends StatelessWidget {
+  const MainAppScreen({required this.initialIndex, super.key});
 
   final int initialIndex;
 
